3c1bc9cb31d1940e3b67162e7c83c3512c1db739
sdesaih <samarthdesain@gmail.com>
2024-04-11 19:19:08 +0530

    STRONG

--- src/Store/passwordGeneratorSlice.js
+++ src/Store/passwordGeneratorSlice.js
@@ -3,14 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 const passwordLength = 25;
 
 const generateNumbers = () => {
-  const array = [0, 1, 2, 3, 4, "A", "a", "B", "b", "S", "s", "<>", "<", ">", 5, 6, 7, 8, 9, "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "[]", "[", "]"];
+  const array = [
+    ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', 
+    '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?', '/'
+  ];
 
   let randomString = "";
   for (let i = 0; i < passwordLength; i++) {
     randomString += array[Math.floor(Math.random() * array.length)];
   }
   return randomString;
-};
+};  
 
 const passwordGeneratorSlice = createSlice({
   name: "password",
